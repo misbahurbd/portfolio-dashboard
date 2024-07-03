028103d6a89a92e38c9769e832d376d5a50c9720
Misbahur Rahman <misbahurbd@gmail.com>
2024-07-03 09:53:08 +0600

    update blog category

--- src/constants/blog.ts
+++ src/constants/blog.ts
@@ -11,4 +11,40 @@ export const blogCategories = [
     label: "ExpressJs",
     value: "ExpressJs",
   },
+  {
+    label: "Node.js",
+    value: "NodeJs",
+  },
+  {
+    label: "JavaScript",
+    value: "JavaScript",
+  },
+  {
+    label: "TypeScript",
+    value: "TypeScript",
+  },
+  {
+    label: "HTML & CSS",
+    value: "HTML_CSS",
+  },
+  {
+    label: "Frontend Development",
+    value: "Frontend",
+  },
+  {
+    label: "Backend Development",
+    value: "Backend",
+  },
+  {
+    label: "Web Development",
+    value: "WebDevelopment",
+  },
+  {
+    label: "UI/UX Design",
+    value: "UI_UX",
+  },
+  {
+    label: "Database Management",
+    value: "Database",
+  },
 ]
