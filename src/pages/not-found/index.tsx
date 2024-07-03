28a92898087b2e6a8f8e3b597705ff6226d6083a
Misbahur Rahman <misbahurbd@gmail.com>
2024-07-03 08:53:18 +0600

    update 404 page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 const NotFoundPage = () => {
   return (
-    <div className="h-screen flex items-center justify-center gap-6">
+    <div className="h-screen flex flex-col items-center justify-center gap-6">
       <PiWarningDuotone className="size-32 text-primary" />
       <h2 className="font-bold text-xl">Oh! Page not found.</h2>
       <Button
